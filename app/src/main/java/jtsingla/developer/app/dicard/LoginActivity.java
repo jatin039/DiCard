25e50a148656dda6bb3f8334f55dc6e345e184de
jtSingla <jatin039@gmail.com>
2016-10-15 23:31:26 +0530

    Initial Commit

--- app/src/main/java/jtsingla/developer/app/dicard/LoginActivity.java
+++ app/src/main/java/jtsingla/developer/app/dicard/LoginActivity.java
@@ -30,6 +30,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+
 import java.util.List;
 
 import static android.Manifest.permission.READ_CONTACTS;
